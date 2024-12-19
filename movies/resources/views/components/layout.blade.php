05c475378de08250f4e08c6222fd559ec38a7345
DILKINIINFO <dilkinikahawalage@gmail.com>
2024-12-18 17:32:48 -1200

    update/delete

--- movies/resources/views/components/layout.blade.php
+++ movies/resources/views/components/layout.blade.php
@@ -8,12 +8,14 @@
     <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+
 </head>
 
-<body class="bg-purple-100 text-slate-900">
-    <header class="bg-slate-800 shadow-lg">
+<body class="bg-purple-300 text-slate-900">
+    <header >
         <nav>
             <a href="{{ route('posts.index') }}" class="nav-link">Home</a>
 
